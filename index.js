b81c4216f84bda0164efe332506bbb55a986fd4e
kadir rodriguez <kadir.rodriguez@ladonware.com>
2024-08-13 15:59:57 -0500

    KR

--- index.js
+++ index.js
@@ -4,23 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 const privateKey = fs.readFileSync('private_key.pem', 'utf8');
 const cors = require('cors'); // Importar cors
 app.use(cors());
 // app.use(bodyParser.json());
-let clientIp
-app.use((req, res, next) => {
-  // Obtener la dirección IP del cliente
-  clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  console.log('Dirección IP del cliente:', clientIp);
-
-  // Continuar con el siguiente middleware o ruta
-  next();
-});
 
 app.get('/', (req, res) => {
-  res.send('Hola, mundo!', clientIp);
+  // Obtener la dirección IP del cliente
+  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  // Enviar la respuesta
+  res.send(`Hola, mundo! Tu IP es: ${clientIp}`);
 });
 
 app.listen(port, () => {
