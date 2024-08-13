01912da0790e861ebded1659c789f2afce5fe47c
kadir rodriguez <kadir.rodriguez@ladonware.com>
2024-08-13 15:38:42 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const crypto = require('crypto');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+const privateKey = fs.readFileSync('private_key.pem', 'utf8');
+const cors = require('cors'); // Importar cors
+app.use(cors());
+// app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  // Obtener la dirección IP del cliente
+  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log('Dirección IP del cliente:', clientIp);
+
+  // Continuar con el siguiente middleware o ruta
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hola, mundo!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
